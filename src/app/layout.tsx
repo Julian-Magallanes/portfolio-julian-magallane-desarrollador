83121b9ea15982be860dd4d19fb45f377d806a33
Julian Magallanes <100171315+Julian-Magallanes@users.noreply.github.com>
2024-08-09 11:15:38 -0300

    fix ant desing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 const montserrat = Montserrat({ subsets: ["latin"] , weight: ["100","200","300","400","500","600","700","800"]});
 
 export const metadata: Metadata = {
@@ -18,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={montserrat.className}>
+      <AntdRegistry>
       <Navbar/>
         {children}
+        </AntdRegistry>
       </body>
     </html>
   );
